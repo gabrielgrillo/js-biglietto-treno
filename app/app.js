5cd324e88b623aee0ec4ab4070877a5e9ffdd04c
gabrielgrillo <gabrielgrillo_4@hotmail.com>
2024-06-29 16:24:26 +0200

    suddivisione problema

--- app/app.js
+++ app/app.js
@@ -1,1 +1,21 @@
+// Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero.
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// va applicato uno sconto del 20% per i minorenni
+// va applicato uno sconto del 40% per gli over 65.
+// Stampate il prezzo del biglietto in console, ma l’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo e questo richiederà un minimo di ricerca.).
+// BONUS:
+// Stampate il risultato anche nella pagina in un apposito elemento <p> del mio codice HTML.
+
+
 console.log("ciao")
+
+//chiedere numero di km da percorrere
+//chiedere età del passeggero
+//prezzo totale viaggio
+    //prezzo al km 0.21€
+    //SE minorenne
+    //sconto 20%
+    //ALTRIMENTI SE over 65 
+    //sconto 40%
+//stampa prezzo finale con due decimali
